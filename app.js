ddbd798d1ee319e95d05422dc47e3e3b104b1cff
xiejj <v-xiejj05@vanke.com>
2018-05-13 15:38:36 +0800

    addBase64Save

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const static = require("koa-static");
 const path = require("path");
 const router = require("./router");
 var cors = require('koa2-cors');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 app.use(cors());
+app.use(bodyParser())
 
 const staticPath = "./static";
 
--- router/index.js
+++ router/index.js
@@ -2,8 +2,9 @@ const router = require("koa-router")();
 const Controller = require("../controller");
 
 module.exports = app => {
-  // 上传图片 放入photo
-  router.post("/photo/:children1?/:children2?", Controller.loadImage);
+    // 上传图片 放入photo
+    router.post("/photo/:children1?/:children2?", Controller.loadImage);
+    router.post("/blog/:children1?/:children2?", Controller.loadBase64);
 
-  app.use(router.routes()).use(router.allowedMethods());
+    app.use(router.routes()).use(router.allowedMethods());
 };
